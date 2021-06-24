b729291c21cdf7b7bd05d28164286f30702dbe58
zxcv098 <1355915720@qq.com>
2021-06-24 11:25:20 +0800

    环境配置

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Button, ConfigProvider } from "antd";
 
-function App() {
+import "antd/dist/antd.less";
+/*
+应用根组件
+*/
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ConfigProvider>
+      {/*全局的antd文案都改变为中文文案*/}
+      <div className="App">
+        <Button type="primary">Button</Button>
+      </div>
+    </ConfigProvider>
   );
-}
+};
 
 export default App;
