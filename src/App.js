0cfed695ac5820d1d17ff3cda260950ecca416b0
zxcv098 <1355915720@qq.com>
2021-06-24 16:58:55 +0800

    环境配置

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { Button, ConfigProvider } from "antd";
 
-import "antd/dist/antd.less";
+//import "antd/dist/antd.less";
 /*
 应用根组件
 */
-const App = () => {
-  return (
-    <ConfigProvider>
-      {/*全局的antd文案都改变为中文文案*/}
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
-    </ConfigProvider>
-  );
-};
+const App: FC = () => (
+  <div>
+    <Button type="primary">Button</Button>
+  </div>
+);
 
 export default App;
